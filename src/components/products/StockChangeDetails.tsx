8817b4b33c9d54d27c0aaa6b34dbefb6d8662ed9
theaarc <109694966+theaarc@users.noreply.github.com>
2025-08-21 04:07:29 +0100

    build error correction

--- src/components/products/StockChangeDetails.tsx
+++ src/components/products/StockChangeDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Badge } from '../common/Badge';
+import Badge from '../common/Badge';
 import { format } from 'date-fns';
 import type { StockChange } from '../../types/models';
 
