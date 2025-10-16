739a82ea61194a6c4015e7e089e781fa88ad15d4
Marco <Vous@exemple.com>
2025-10-16 19:51:14 +0100

    pop

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-13fdf7ef'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s0pvmr4lj6o"
+    "revision": "0.gnp7003tsgg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
